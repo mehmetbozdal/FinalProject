e430aa555e97eda1bd86209935c7cbd7ce399641
mehmet bozdal <mehmetbozdal@hotmail.com>
2021-08-30 09:55:01 +0300

    8. Ders Sonu

---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context: Db tabloları ile proje classlarını bağlamak
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+        public DbSet<Product> Products { get; set; } //Product Products a bağla
+        public DbSet<Category> Categories { get; set; } //Category yi Categories e bağla
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
