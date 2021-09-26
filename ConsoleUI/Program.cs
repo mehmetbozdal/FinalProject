b7a9c62ab9f51390e80f97a88ec5c1a824579487
mehmet bozdal <mehmetbozdal@hotmail.com>
2021-09-26 15:51:58 +0300

    10. Lesson

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -27,10 +27,19 @@ namespace ConsoleUI
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetProductDetails()) //GetAll kısmını hangisini istiyorsan ona çevirebilirsin. Getall hepsini, GetAllByCategory parantez içine hangi kategori ise yaz vb.
+            var result = productManager.GetProductDetails();
+            if (result.Success == true)
             {
-                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+            
         }
     }
 }
